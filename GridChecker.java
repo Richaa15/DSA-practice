6e3f8b2b536a95e46d75a06097e1c9d3755d7140
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-06-20 19:38:58 +0530

    Update GridChecker.java

--- GridChecker.java
+++ GridChecker.java
@@ -1,4 +1,6 @@
-
+//given a square grid of characters in the range ascii(a-z) , rearrange elements of each row alphabetically ascending. 
+//Determine if the columns are also in alphabetical ascending order top to bottom. Return yes if they are or no if they are not.
+//Write a java code for the same
 
 public class GridChecker {
     public static String checkGrid(char[][] grid) {

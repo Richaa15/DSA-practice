6a1a9d5455d23637450e5e43ea01e96d2f538bf6
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-07-07 10:19:54 +0530

    Update NRepeated.java

--- NRepeared.java
+++ NRepeared.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class NRepeated{
-    public int notUnique(int arr[], int n){
+    public static int notUnique(int nums[], int n){
         
         int c = nums.length / 2;
 
@@ -15,32 +15,30 @@ public class NRepeated{
                 return nums[i];
             }
         }
-
         // If no repeated element found
         return -1;
     }
-    
-    
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array(2n): ");
         int n = sc.nextInt();
+        System.out.println("Enter the array: ");
+        int arr[] = new int[n];
         if(n%2==0){
-            System.out.println("Enter the array: ");
-            int arr[] = new int[n];
             for(int i = 0; i<n; i++){
                 arr[i] = sc.nextInt();
-            }else{
-                System.out.println("Please enter valid n.")
             }
         }
+        else{
+            System.out.println("Please enter valid n.");
+        }
         
-        int r = NRepeated(nums);
+        
+        int r = notUnique(arr,n);
         if (r != -1) {
             System.out.println("The element that has repeated n times: " + r);
         } else {
             System.out.println("No repeated element found.");
         }
-        
     }
 }
